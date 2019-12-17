9cdff1a4dda2c32a868cc0359117ed1764c074cf
IsseiMiyamoto <ants.can.walk@gmail.com>
2019-12-17 16:55:35 +0900

    service_usersテーブルを作成しました

---
+++ database/migrations/2019_12_17_074313_create_service_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServiceUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('u_id')->unique();
+            $table->string('u_name');
+            $table->string('u_pw');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_users');
+    }
+}
