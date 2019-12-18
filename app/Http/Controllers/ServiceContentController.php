10d8501d3ae539bf9e8ea55376e247708791f9ec
shihori <shihori.23@gmail.com>
2019-12-19 00:55:37 +0900

    前回のコマの情報を取得

--- app/Http/Controllers/ServiceContentController.php
+++ app/Http/Controllers/ServiceContentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ServiceContent;
+use Auth;
+
 class ServiceContentController extends Controller
 {
     //コマデータ取得
@@ -13,4 +15,38 @@ class ServiceContentController extends Controller
                 'comas' => $comas
             ]);
     }
+
+    //coma_create.blade.phpの前回のコマを表示する
+    public function comaPev(){
+        // コマのデータを降順で１レコード取得
+        $comas = ServiceContent::orderBy('c_id','dec')->first();
+        return view('coma_create',[
+        'comas' => $comas
+        ]);
+    }
+
+    //コマデータ取得
+    public function comaSave(Request $request){
+
+        
+        $img = $request->data; //画像データの取得
+        $imgPevNum = $request->imgNamePev; // 前コマのc_idの取得
+        
+        $img = str_replace('data:image/png;base64,', '', $img);
+        $img = str_replace(' ', '+', $img);
+        $fileData = base64_decode($img);
+        //画像保存ファイル名を前のコマのc_idから生成
+        $imgNum = $imgPevNum + 1;
+        $fileName = 'img/'.$imgNum .'.png';
+        file_put_contents($fileName, $fileData);
+
+        //DBにファイル名とユーザーIDをインサート
+        $images = new ServiceContent;
+        $images->insert([
+                'img_file' => $fileName,
+                'u_id' => 'aaa'
+             ]);
+        return redirect('/');     
+    }
+
 }
