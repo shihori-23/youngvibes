3f224e309fac0fdf012ee69ec97e1718568f3465
yu <yu@gmail.com>
2019-12-18 17:18:14 +0900

    Forget your password?を日本語にした（テストagain)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                                    パスワードを忘れましたか?
                                 </a>
                             </div>
                         </div>
