12c86fd6150550424155a7d61eacd84081ccba26
shihori <shihori.23@gmail.com>
2019-12-20 10:36:05 +0900

    ヘッダーファイルの追加

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Document</title>
+    <title>Chumugu</title>
     <link rel="stylesheet" href="{{asset('/css/reset.css')}}" />
     <link rel="stylesheet" href="{{asset('/css/header.css')}}" />
   </head>
   <body> --}}
     <header>
-      <div class="header-logo">Chumugu</div>
+      <div class="header-logo"><img class="header_logo_img" src="{{asset('/img/logo/Chumugu_logo.png')}}"></div>
       <ul class="header-menu">
         <li>作品をみる</li>
         <li>作品を作る</li>
