da2a787d57cd7ac2f588b487a4b91b6a83189ecc
IsseiMiyamoto <ants.can.walk@gmail.com>
2019-12-23 11:44:19 +0900

    ログイン未認証を弾き倒す

--- app/Http/Controllers/ServiceContentController.php
+++ app/Http/Controllers/ServiceContentController.php
@@ -9,53 +9,68 @@ use Auth;
 class ServiceContentController extends Controller
 {
     //topページでコマデータ取得
-    public function comaGet(){
-            $comas = ServiceContent::orderBy('id','dec')->get();
-            return view('top',[
+    public function comaGet()
+    {
+        if (Auth::check()) {
+            $comas = ServiceContent::orderBy('id', 'dec')->get();
+            return view('top', [
                 'comas' => $comas
             ]);
+        } else {
+            return redirect('login');
+        }
     }
 
     //long_storyページでコマデータ取得
-    public function comaGetToLong(){
-            $comas = ServiceContent::orderBy('id','dec')->get();
-            return view('long_story',[
+    public function comaGetToLong()
+    {
+        if (Auth::check()) {
+            $comas = ServiceContent::orderBy('id', 'dec')->get();
+            return view('long_story', [
                 'comas' => $comas
             ]);
+        } else {
+            return redirect('login');
+        }
     }
 
     //coma_create.blade.phpの前回のコマを表示する
-    public function comaPev(){
-        // コマのデータを降順で１レコード取得
-        $comas = ServiceContent::orderBy('id','dec')->first();
-        return view('coma_create',[
-        'comas' => $comas
-        ]);
+    public function comaPev()
+    {
+        if (Auth::check()) {
+            // コマのデータを降順で１レコード取得
+            $comas = ServiceContent::orderBy('id', 'dec')->first();
+            return view('coma_create', [
+                'comas' => $comas
+            ]);
+        } else {
+            return redirect('login');
+        }
     }
 
     //コマデータ取得
-    public function comaSave(Request $request){
- 
+    public function comaSave(Request $request)
+    {
+
         $img = $request->data; //画像データの取得
         $imgPevNum = $request->imgNamePev; // 前コマのidの取得
         $imgEmail = $request->imgEmail; // 前コマのemailの取得
-        
+
         $img = str_replace('data:image/png;base64,', '', $img);
         $img = str_replace(' ', '+', $img);
         $fileData = base64_decode($img);
         //画像保存ファイル名を前のコマのc_idから生成
         $imgNum = $imgPevNum + 1;
-        $fileName = 'img/coma/c_'.$imgNum .'.png';
+        $fileName = 'img/coma/c_' . $imgNum . '.png';
         file_put_contents($fileName, $fileData);
 
         //DBにファイル名とユーザーIDをインサート
         $images = new ServiceContent;
-        $postImageName = 'c_'.$imgNum .'.png';
+        $postImageName = 'c_' . $imgNum . '.png';
         $images->insert([
-                'img_file' => $postImageName,
-                'email' => $imgEmail
-             ]);
-        return redirect('/top');     
+            'img_file' => $postImageName,
+            'email' => $imgEmail
+        ]);
+        return redirect('/top');
     }
-    
-    }
+}
